857aa11ad9dfd2500f0fb1c42172727faf9fbecc
Rajalakshmi-Girish <rajalakshmi.girish1@ibm.com>
2024-11-07 04:40:37 -0800

    Add TF code to import PowerVS catalog image

--- data/powervs/main.tf
+++ data/powervs/main.tf
@@ -12,7 +12,35 @@ resource "ibm_pi_network" "public_network" {
   pi_dns = [ "8.8.4.4", "8.8.8.8"]
 }
 
+data "ibm_pi_catalog_images" "catalog_images" {
+  pi_cloud_instance_id = var.powervs_service_id
+}
+data "ibm_pi_images" "project_images" {
+  pi_cloud_instance_id = var.powervs_service_id
+}
+
+locals {
+  catalog_power_image = [for x in data.ibm_pi_catalog_images.catalog_images.images : x if x.name == var.powervs_image_name]
+  project_power_image = [for x in data.ibm_pi_images.project_images.image_info : x if x.name == var.powervs_image_name]
+  invalid_power_image = length(local.project_power_image) == 0 && length(local.catalog_power_image) == 0
+  # If invalid then use name to fail in ibm_pi_instance resource; else if not found in project then import using ibm_pi_image; else use the power image id
+  power_image_id = (
+    local.invalid_power_image ? var.powervs_image_name : (
+      length(local.project_power_image) == 0 ? ibm_pi_image.power[0].image_id : local.project_power_image[0].id
+    )
+  )
+}
+
+# Copy image from catalog if not in the project and present in catalog
+resource "ibm_pi_image" "power" {
+  count                = length(local.project_power_image) == 0 && length(local.catalog_power_image) == 1 ? 1 : 0
+  pi_image_name        = var.powervs_image_name
+  pi_image_id          = local.catalog_power_image[0].image_id
+  pi_cloud_instance_id = var.powervs_service_id
+}
+
 module "master" {
+  depends_on = [ibm_pi_image.power]
   source = "./instance"
 
   ibmcloud_api_key = var.powervs_api_key
@@ -28,6 +56,7 @@ module "master" {
 }
 
 module "workers" {
+  depends_on = [ibm_pi_image.power]
   source = "./instance"
   instance_count = var.workers_count
 
